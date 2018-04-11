e4f60be17d3db8b8d45b76697c0ce888fe5f4bce
liangyanxiang <liangyanxiang@juzix.io>
2018-04-11 15:18:31 +0800

    deve

--- static/js/cookie.js
+++ static/js/cookie.js
@@ -1,25 +1,26 @@
-/*
- * 作者：梁燕翔
- * 时间：2017/04/11
- * 功能：对cookie的操作
- */
 
 export default {
-  get(cname) {
-    let name = cname + "=";
-    let ca = document.cookie.split(';');
-    for(let i = 0; i < ca.length; i++) {
-      let c = ca[i].trim();
-      if(c.indexOf(name) == 0) {
-        return c.substring(name.length, c.length);
-      }
+    get(cname) {
+        let name = cname + "=";
+        let ca = document.cookie.split(';');
+        for (let i = 0; i < ca.length; i++) {
+            let c = ca[i].trim();
+            if (c.indexOf(name) == 0) {
+                return c.substring(name.length, c.length);
+            }
+        }
+        return "";
+    },
+    set(cname, cvalue, ms) {
+        let d = new Date();
+        d.setTime(d.getTime() + ms);
+        let expires = "expires=" + d.toGMTString();
+        document.cookie = cname + "=" + cvalue + "; " + expires;
+    },
+    del(name) {
+        var exp = new Date();
+        exp.setTime(exp.getTime() + (-1 * 24 * 60 * 60 * 1000));
+        var cval = this.get(name);
+        document.cookie = name + "=" + cval + "; expires=" + exp.toGMTString();
     }
-    return "";
-  },
-  set(cname, cvalue, ms) {
-    let d = new Date();
-    d.setTime(d.getTime() + ms);
-    let expires = "expires=" + d.toGMTString();
-    document.cookie = cname + "=" + cvalue + "; " + expires;
-  }
 }
