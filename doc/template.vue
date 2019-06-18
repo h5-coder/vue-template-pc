1a7866851ebae6bd7e5bf477de68fbcd2d0af7d0
liangyanxiang <liangyanxiangde@163.com>
2019-06-19 00:40:01 +0800

    添加文档

---
+++ doc/template.vue
@@ -0,0 +1,62 @@
+<template>
+    <div class="">
+
+    </div>
+</template>
+
+<script>
+    //import  from ''
+
+    export default {
+        //组件名
+        name: 'template',
+        //实例的数据对象
+        data() {
+            return {
+
+            }
+        },
+        //数组或对象，用于接收来自父组件的数据
+        props: {
+
+        },
+        //计算
+        computed: {
+
+        },
+        //方法
+        methods: {
+
+        },
+        //生命周期函数
+        created() {
+
+        },
+        beforeMount() {
+
+        },
+        mounted() {
+
+        },
+        //监视
+        watch: {
+
+        },
+        //组件
+        components: {
+
+        },
+        //过滤器
+        filters:{
+
+        },
+        //自定义指令
+        directive:{
+
+        }
+    }
+</script>
+
+<style lang="less" scoped>
+
+</style>
